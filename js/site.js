4518fc414f237d7d39d03265b591da5cc3129268
brianley99 <Brianley99@gmail.com>
2023-04-16 23:04:28 -0700

    add single page index

--- js/site.js
+++ js/site.js
@@ -1,5 +1,5 @@
 //Controller
-function getValues(){
+function getValues() {
     //Get Inputs From DOM
     let userString = document.getElementById('userString').value;
 
@@ -7,11 +7,11 @@ function getValues(){
     let palindromeBool = isPallindrome(userString);
 
     //Display result on page
-    displayResult(palindromeBool);
+    displayResult(userString, palindromeBool);
 }
 
 //Business Logic
-function isPallindrome(userString){
+function isPallindrome(userString) {
 
     //strip userString to raw letters
     let rawString = userString.toLowerCase();
@@ -20,7 +20,7 @@ function isPallindrome(userString){
 
     //reverse userString and store into new variable
     let reversedString = '';
-    for (let index = rawString.length -1; index >= 0; index--) {
+    for (let index = rawString.length - 1; index >= 0; index--) {
         reversedString += rawString[index];
     };
 
@@ -33,7 +33,15 @@ function isPallindrome(userString){
 }
 
 //View
-function displayResult(isPallindrome){
+function displayResult(userString ,palindromeBool) {
     //Recive a boolean value, display on page
-    document.getElementById('results').innerText = isPallindrome;
+
+    if (palindromeBool == true) {
+        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userString}</span> is a Palindrome`;
+        document.getElementById('resultsAlert').classList.add('alert-success');
+    } else {
+        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userString}</span> is not a Palindrome`;
+        document.getElementById('resultsAlert').classList.add('alert-danger');
+    }
+    document.getElementById('resultsAlert').classList.remove('d-none');
 }
