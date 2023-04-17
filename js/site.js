94c247f1718be0af99a6b119117475a000b89f04
brianley99 <Brianley99@gmail.com>
2023-04-16 21:36:30 -0700

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,10 +1,39 @@
-function displayMessage() {
-    let msg = document.getElementById('message').value;
-    //alert(msg);
+//Controller
+function getValues(){
+    //Get Inputs From DOM
+    let userString = document.getElementById('userString').value;
 
-    Swal.fire({
-        backdrop: false,
-        title: 'APP Name',
-        text: msg
-    })
+    //Check if its a pallindrome
+    let palindromeBool = isPallindrome(userString);
+
+    //Display result on page
+    displayResult(palindromeBool);
+}
+
+//Business Logic
+function isPallindrome(userString){
+
+    //strip userString to raw letters
+    let rawString = userString.toLowerCase();
+    let regx = /[^a-z0-9]/gi;
+    rawString = rawString.replace(regx, "");
+
+    //reverse userString and store into new variable
+    let reversedString = '';
+    for (let index = rawString.length -1; index >= 0; index--) {
+        reversedString += rawString[index];
+    };
+
+    //compare if two are the same & return result
+    if (rawString == reversedString) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//View
+function displayResult(isPallindrome){
+    //Recive a boolean value, display on page
+    document.getElementById('results').innerText = isPallindrome;
 }
